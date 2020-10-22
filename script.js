863a6229065c9f8495f0190016f81cd735d34240
Akib Mohaimenur Rahman <akib.rhast@gmail.com>
2020-10-22 12:45:25 -0400

    Some design changes

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const gameContainer = document.getElementById("game");
-
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
-];
+let base_poke_url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+let board_size = 5;
+let poke_id = create_poke_id_array(5)
+let card1 = null;
+let card2 = null;
+let cardsFlipped = 0;
+let noClicking = false;
 
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
@@ -36,18 +30,18 @@ function shuffle(array) {
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
+let shuffledPokeIds = shuffle(poke_id);
 
 // this function loops over the array of colors
 // it creates a new div and gives it a class with the value of the color
 // it also adds an event listener for a click for each card
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
+function createDivsForColors(emojiArray) {
+  for (let emoji of emojiArray) {
     // create a new div
     const newDiv = document.createElement("div");
 
     // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
+    newDiv.classList.add(emoji);
 
     // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
@@ -58,10 +52,75 @@ function createDivsForColors(colorArray) {
 }
 
 // TODO: Implement this function!
-function handleCardClick(event) {
+function handleCardClick(e) {
   // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
+  console.log("you just clicked", e.target);
+  if (noClicking === true) return;
+  if (e.target.classList.contains('flipped')) return;
+
+  // Card click listener
+  let currentCard = e.target;
+  let emoji = currentCard.classList[0];
+  currentCard.style.backgroundImage = `url(${base_poke_url}${emoji}.png)`;
+
+  if (!card1 || !card2) {
+    currentCard.classList.add("flipped");
+    card1 = card1 || currentCard;
+    card2 = currentCard === card1 ? null : currentCard;
+  }
+
+  if (card1 && card2) {
+    noClicking = true;
+    let gif1 = card1.className;
+    let gif2 = card2.className;
+
+    if (gif1 === gif2) {
+      cardsFlipped += 2;
+      card1.removeEventListener("click", handleCardClick);
+      card2.removeEventListener("click", handleCardClick);
+      card1 = null;
+      card2 = null;
+      noClicking = false;
+    } else {
+      setTimeout(function() {
+        card1.style.backgroundImage  = '';
+        card2.style.backgroundImage  = '';
+        card1.classList.remove('flipped');
+        card2.classList.remove('flipped');
+        card1 = null;
+        card2 = null;
+        noClicking = false;
+      }, 1000)
+    }
+  }
+
+  if (cardsFlipped === poke_id.length) alert('Winner!!!');
+
 }
 
+//A board_size = 5 would produce a 5x5 board
+function create_poke_id_array(board_size){
+  var poke_array = []
+  for(let i=0;i<board_size;i++){
+
+
+    poke_array.push(getRandomIntInclusive(1,100));
+  
+  }
+  poke_array  = poke_array.reduce(function (res, current, index, array) {
+    return res.concat([current, current]);
+  }, []);
+  return poke_array;
+}
+
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+
 // when the DOM loads
-createDivsForColors(shuffledColors);
+createDivsForColors(shuffledPokeIds);
+
